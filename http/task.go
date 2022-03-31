9ec0c40274bbd6dbc0e8c33e74e07dc7b0258347
clauRamirez <clau.rjimenez@gmail.com>
2022-03-31 11:51:00 +0100

    handlers return JSON instead of panic on error

--- http/task.go
+++ http/task.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"go-api-test-2/models"
@@ -8,12 +9,11 @@ import (
 	"strconv"
 )
 
-// TO-DO: use respondWithError instead of panicking on error
-
 func (h *Handler) tasks(w http.ResponseWriter, r *http.Request) {
 	ts, err := h.TaskService.GetAll()
 	if err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid request")
+		return
 	}
 	respondWithJSON(w, http.StatusOK, ts)
 }
@@ -22,10 +22,22 @@ func (h *Handler) task(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
 	id, err := strconv.Atoi(v["id"])
 	if err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid item id")
+		return
 	}
 
 	t, err := h.TaskService.Get(id)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "item not found")
+			return
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
 	respondWithJSON(w, http.StatusOK, t)
 }
 
@@ -33,26 +45,30 @@ func (h *Handler) deleteTask(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
 	id, err := strconv.Atoi(v["id"])
 	if err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid item id")
+		return
 	}
 
 	if err = h.TaskService.Delete(id); err != nil {
-		panic(err)
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	respondWithJSON(w, http.StatusOK, map[string]string{"response": "success"})
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func (h *Handler) createTask(w http.ResponseWriter, r *http.Request) {
 	var t models.Task
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&t); err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid request")
+		return
 	}
 	r.Body.Close()
 
 	if err := h.TaskService.Create(&t); err != nil {
-		panic(err)
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	respondWithJSON(w, http.StatusOK, t)
@@ -62,21 +78,24 @@ func (h *Handler) updateTask(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
 	id, err := strconv.Atoi(v["id"])
 	if err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid item id")
+		return
 	}
 
 	var t models.Task
 
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&t); err != nil {
-		panic(err)
+		respondWithError(w, http.StatusBadRequest, "invalid item payload")
+		return
 	}
 	r.Body.Close()
 
 	t.Id = id
 
 	if err := h.TaskService.Update(t); err != nil {
-		panic(err)
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	respondWithJSON(w, http.StatusOK, t)
